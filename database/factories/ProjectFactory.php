e67309561b5a4809ff2e059f9b59e32eb31f0ff9
“lfrclnando” <lfernando.div@gmail.com>
2024-11-04 18:53:25 -0300

    Feat/Create Proposals Model, migrations & seeders

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -23,7 +23,8 @@ class ProjectFactory extends Factory
             'description' => fake()->randomHtml(),
             'ends_at' => fake()->dateTimeBetween('now', '+ 3 days'),
             'status' => fake()->randomElement(['open', 'closed']),
-            'tech_stack' => fake()->randomElements(['react', 'php', 'laravel', 'vue', 'tailwind', 'css', 'javascript', 'python', 'nextjs'], random_int(1, 5)),
+            'tech_stack' => fake()->randomElements(['react', 'php', 'laravel', 'vue', 'tailwind', 'css', 'javascript', 
+                'python', 'nextjs'], random_int(1, 5)),
             'created_by' => User::factory(),
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Proposal;
 use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,7 +19,10 @@ class DatabaseSeeder extends Seeder
         User::factory()->count(200)->create();
 
         User::query()->inRandomOrder()->limit(10)->get()
-            ->each(fn (User $u) => Project::factory()->create([
-                'created_by' => $u->id]));
+            ->each(function (User $u) {
+                $project = Project::factory()->create(['created_by' => $u->id]);
+
+                Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+            });
     }
 }
