3193620a33db179f5d110b3066cbab21a51ad40b
“lfrclnando” <lfernando.div@gmail.com>
2024-11-07 10:00:26 -0300

    Feat/Notifying Proposal Component that a new proposal has been created

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -36,6 +36,7 @@ class Create extends Component
                 ['hours' => $this->hours],
             );
 
+        $this->dispatch('proposal::created'); 
         $this->modal = false;
     }
     public function render()
